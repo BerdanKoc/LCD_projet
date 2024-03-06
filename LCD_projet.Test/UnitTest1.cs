6fcea4ab1af8b5df8c5ae29ba2e2254c98aff888
BerdanKoc <Berdan.koc@epsi.fr>
2024-03-06 14:54:14 +0100

    Affichage du chiffre 1 en ASCII-art sytle LCD dans la console (sans le test)

---
+++ LCD_projet.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace LCD_projet.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ LCD_projet/Nombres.cs
@@ -0,0 +1,10 @@
+﻿namespace LCD_projet
+{
+    public static class Nombres
+    {
+        public static readonly string Un =
+            "   " + Environment.NewLine +
+            "  |" + Environment.NewLine +
+            "  |";
+    }
+}
